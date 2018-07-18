945d385e9acf432acc55f9955be911733a9794c1
Miguel Picornell <mpicornell@optaros.com>
2018-07-18 14:06:46 -0400

    added linked_fi codes for loading as well

--- inspired_csv_to_json.py
+++ inspired_csv_to_json.py
@@ -1,8 +1,11 @@
 import csv
-import json
-#import rapidjson as json
+import rapidjson as json
 
-#jsonfile = open('mcc_codes.json', 'w')
+with open('inspired_linked_fi_codes.csv', 'r', encoding='utf-8-sig') as csvfile:
+    reader = csv.DictReader(csvfile)
+    out = json.dumps([ row for row in reader ], indent=4)
+    with open('inspired_linked_fi_codes.json', 'w') as jsonfile:
+        jsonfile.write(out)
 
 with open('inspired_mcc_codes.csv', 'r', encoding='utf-8-sig') as csvfile:
     reader = csv.DictReader(csvfile)
